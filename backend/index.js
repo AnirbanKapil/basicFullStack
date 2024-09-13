da2ed3dcfb079ac2a1e611647a8606e762497a5f
Anirban Kapil <anirbankapil@gmail.com>
2024-09-14 00:58:49 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import express from "express";
 const app = express();
 
 
-app.get("/jokes",(req,res)=>{
+app.get("/api/jokes",(req,res)=>{
     const jokes = [
         {
           id: 1,
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,32 @@
 
+import { useEffect, useState } from 'react'
+import axios from "axios";
 import './App.css'
+
+
 
 function App() {
 
+  const [jokes,setJokes] = useState([]);
+  
+  useEffect(()=>{
+     axios.get("/api/jokes")
+     .then((response)=>{
+        setJokes(response.data)
+     })
+     .catch((error)=> console.log(error));
+     
+  },[])
+
   return (
     <>
-      <h1>Hello</h1>
+    <h2>No of jokes {jokes.length}</h2>
+    {jokes.map((joke)=> (
+      <div key={joke.id}>
+        <h3>{joke.title}</h3>
+        <p>{joke.content}</p>
+      </div> 
+    ))}  
     </>
   )
 }
